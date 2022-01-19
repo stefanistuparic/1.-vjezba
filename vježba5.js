41a9e41fb4f61ba234c3f7cb70ab56bb4cb45759
stefani.stuparic <75335116+stefanistuparic@users.noreply.github.com>
2022-01-19 19:35:20 +0100

    Vježbe JS

---
+++ vježba5.js
@@ -0,0 +1,7 @@
+var imena = ["Ivana", "Sanjin" , "Marijan" , "Doroti" , "Damir" , "Lorena" , "Ružica"];
+imena.forEach(element => console.log(element));
+
+var slijed = [["dijete" , "odrasla osoba"] , ["računalo" , "pisača mašina"] , ["mokro" , "suho"]];
+console.table(slijed);
+slijed.push(["gladan" , "sit"]);
+console.table(slijed);
